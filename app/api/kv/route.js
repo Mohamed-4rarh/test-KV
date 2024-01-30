22bd60e037d1fb4dfa0e9dab5fbd50be22f597fa
mohamed-4rarh <mainmohedadham@gmail.com>
2024-01-30 22:42:14 +0200

    add user name

--- app/api/kv/route.js
+++ app/api/kv/route.js
@@ -2,6 +2,6 @@
 export const runtime = 'edge'
 
 export async function GET(context) {
-    const user = await context.env.domains_structure_01.get("user-id")
+    const user = await context.env.domains_structure_01.get("user-name")
     return new Response("userID: " + user)
 }
