9856da5585107eb16a6a9bbb42c87883b1530949
mohamed-4rarh <mainmohedadham@gmail.com>
2024-01-30 22:37:00 +0200

    edit

--- app/api/kv/route.js
+++ app/api/kv/route.js
@@ -2,6 +2,6 @@
 export const runtime = 'edge'
 
 export async function GET() {
-    const user = await process.env.domains_structure_01.get("user-id")
+    const user = await process.env.domains_structure_KV.get("user-id")
     return new Response("userID: " + user)
 }
